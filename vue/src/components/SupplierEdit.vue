f3972e92dfce5f4d639579068fb1b539e14c6636
Marcin Koziarski <koziarskim@yahoo.com>
2019-02-22 14:04:08 -0600

    Modify supplier view. Add data/state.

--- vue/src/components/SupplierEdit.vue
+++ vue/src/components/SupplierEdit.vue
@@ -12,13 +12,43 @@
                 <label class="top-label">Name:</label>
                 <input class="form-control" type="text" v-model="supplier.name" placeholder="Name"/>
             </b-col>
+            <b-col cols=3>
+                <label class="top-label">Account #:</label>
+                <input class="form-control" type="text" v-model="supplier.account" placeholder="Account #"/>
+            </b-col>
+            <b-col cols=2>
+                <label class="top-label">Pay Terms:</label>
+                <input class="form-control" type="text" v-model="supplier.paymentTerms" placeholder="Payment Terms"/>
+            </b-col>
+        </b-row>
+        <b-row>
             <b-col cols=4>
                 <label class="top-label">Address:</label>
                 <input class="form-control" type="text" v-model="supplier.address" placeholder="Street and number"/>
             </b-col>
-            <b-col cols=4>
+            <b-col cols=2 offset=3>
+                <label class="top-label">Freight Terms:</label>
+                <input class="form-control" type="text" v-model="supplier.freightTerms" placeholder="Freight Terms"/>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col cols=2>
                 <label class="top-label">City:</label>
                 <input class="form-control" type="text" v-model="supplier.city" placeholder="City"/>
+            </b-col>
+            <b-col cols=1>
+                <label class="top-label">State:</label>
+                <input class="form-control" type="text" v-model="supplier.state"/>
+            </b-col>
+            <b-col cols=2>
+                <label class="top-label">Zip Code:</label>
+                <input class="form-control" type="text" v-model="supplier.zip" placeholder="Zip code"/>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col cols=3>
+                <label class="top-label">Phone:</label>
+                <input class="form-control" type="text" v-model="supplier.phone" placeholder="Phone"/>
             </b-col>
         </b-row>
     </b-container>
@@ -27,7 +57,7 @@
 <script>
 import http from "../http-common";
 import router from "../router";
-import httpUtils from "../httpUtils";
+import state from "../data/state";
 
 export default {
   name: "add-component",
@@ -43,7 +73,9 @@ export default {
         phone: "",
         paymentTerms: "",
         freightTerms: ""
-      }
+      },
+      selected: "",
+      availableStates: state.states,
     };
   },
   computed: {},
