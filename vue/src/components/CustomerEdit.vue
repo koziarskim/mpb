ba81a2edeb357f3c59f70c916401f3e6be61488f
Marcin Koziarski <koziarskim@yahoo.com>
2019-03-01 09:26:22 -0600

    Layout of CustomerEdit.vue

--- vue/src/components/CustomerEdit.vue
+++ vue/src/components/CustomerEdit.vue
@@ -8,79 +8,82 @@
             </div>
         </div>
         <b-row>
-            <b-col cols=8>
+            <b-col cols=6>
                 <b-row>
-                    <b-col cols=4>
+                    <b-col cols=7>
                         <label class="top-label">Name:</label>
                         <input class="form-control" type="search" v-model="customer.name" placeholder="Name"/>
                     </b-col>
-                    <b-col cols=3>
+                    <b-col cols=5>
                         <label class="top-label">Account #:</label>
                         <input class="form-control" type="text" v-model="customer.account" placeholder="Account #"/>
                     </b-col>
                 </b-row>
                 <b-row>
-                    <b-col cols=2>
+                    <b-col cols=4>
                         <label class="top-label">Pay Terms:</label>
                         <input class="form-control" type="text" v-model="customer.paymentTerms" placeholder="Payment Terms"/>
                     </b-col>
-                    <b-col cols=2>
+                    <b-col cols=4>
                         <label class="top-label">Freight Terms:</label>
                         <b-select option-value="id" option-text="name" :list="availableFreights" v-model="freightTerms" placeholder="Freight terms"></b-select>
                     </b-col>
                 </b-row>
                 <b-row>
-                    <b-col cols=3>
+                    <b-col cols=6>
                         <label class="top-label">Phone:</label>
                         <input class="form-control" type="text" v-model="customer.phone" placeholder="Phone"/>
                     </b-col>
-                    <b-col cols=3>
+                    <b-col cols=6>
                         <label class="top-label">Phone 2:</label>
                         <input class="form-control" type="text" v-model="customer.phone2" placeholder="Phone 2"/>
                     </b-col>
                 </b-row>
                 <b-row>            
-                    <b-col cols=3>
+                    <b-col cols=6>
                         <label class="top-label">Email:</label>
                         <input class="form-control" type="text" v-model="customer.email" placeholder="Email"/>
                     </b-col>
-                    <b-col cols=3>
+                    <b-col cols=6>
                         <label class="top-label">Email 2:</label>
                         <input class="form-control" type="text" v-model="customer.email2" placeholder="Email 2"/>
                     </b-col>
                 </b-row>
                 <b-row>
-                    <b-col cols=4>
+                    <b-col cols=6>
                         <label class="top-label">Contact:</label>
                         <input class="form-control" type="text" v-model="customer.contactName" placeholder="Contact Name"/>
                     </b-col>       
                 </b-row>
             </b-col>
-            <b-col>
+            <b-col cols=5>
                 <b-row>
-                    <b-col cols=8>
-                        <label v-if="customer.addresses.length > 0 && !addressEditFlag" class="top-label">Default Address:</label>
+                    <b-col cols=10>
+                        <label v-if="!addressEditFlag" class="top-label">Default Address:</label>
                         <b-select v-if="customer.addresses.length > 0 && !addressEditFlag" option-value="id" option-text="street" :list="customer.addresses" v-model="address" placeholder="Address"></b-select>
-                        <label v-if="customer.addresses.length == 0 ||  addressEditFlag" class="top-label">Street:</label>
+                        <label v-if="addressEditFlag" class="top-label">Street:</label>
                         <input v-if="customer.addresses.length == 0 ||  addressEditFlag" class="form-control" type="search" v-model="address.street" placeholder="Street"/>
                     </b-col>
-                    <b-col cols=4>
-                        <b-button v-if="!addressEditFlag" variant="link" @click="addAddress()">add</b-button>
-                        <b-button v-if="addressEditFlag" variant="link" @click="saveAddress()">save</b-button>
+                    <b-col cols=2 style="padding-top: 20px; padding-left: 0px">
+                        <b-button v-if="!addressEditFlag" variant="link" @click="addAddress()"><span>Add</span></b-button>
+                        <b-button v-if="addressEditFlag" variant="link" @click="saveAddress()"><span>Save</span></b-button>
                     </b-col>
                 </b-row>
                 <b-row>
-                    <!-- <b-row v-if="addressEditFlag || address"> -->
-                        <!-- <b-col>
-                            <b-form-checkbox :disabled="address.defaultFlag" v-model="address.defaultFlag" @change="unsetDefaultAddress"><label class="top-label">Default Address:</label></b-form-checkbox>
-                        </b-col> -->
                 </b-row>
                 <b-row>
-                        <b-col>
-                            <label class="top-label">City:</label>
-                            <input class="form-control" type="search" v-model="address.city" placeholder="City"/>
-                        </b-col>
-                    <!-- </div> -->
+                    <b-col cols=7>
+                        <label class="top-label">City:</label>
+                        <input class="form-control" type="tel" v-model="address.city" placeholder="City"/>
+                    </b-col>
+                    <b-col cols=2>
+                        <label class="top-label">State:</label>
+                        <input class="form-control" type="tel" v-model="address.state" placeholder=""/>
+                    </b-col>
+                    <b-col cols=3>
+                        <label class="top-label">Zip Code:</label>
+                        <input class="form-control" type="tel" v-model="address.zip" placeholder="Zip"/>
+                    </b-col>
                 </b-row>
             </b-col>
         </b-row>
