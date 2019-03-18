052226366c330560742a6c85e8936c4467cad9c1
Marcin Koziarski <koziarskim@yahoo.com>
2019-03-18 17:46:56 -0500

    Fix item removal on sale.

--- vue/src/components/SaleEdit.vue
+++ vue/src/components/SaleEdit.vue
@@ -1,95 +1,91 @@
 <template>
-    <b-container fluid>
-        <div style="border: 0px" class="d-flex justify-content-between align-items-center">
-            <h4 style="text-align: left;">Sale Order:</h4>
-            <div style="text-align: right;">
-                <!-- <b-button type="submit" variant="primary" @click="saveSale">Save</b-button> -->
-                <b-button type="reset" variant="success" @click="saveAndClose">Save & Close</b-button>
-            </div>
-        </div>
-        <b-row>
-            <b-col cols=4>
-                <label class="top-label">Customer:</label>
-                <b-select option-value="id" option-text="name" :list="availableCustomers" v-model="customer" placeholder="Customer"></b-select>
-            </b-col>
-            <b-col cols=2>
-                <label class="top-label">Date:</label>
-                <input class="form-control" type="tel" v-model="sale.date" placeholder="Date"/>
-            </b-col>
-            <b-col cols=2>
-                <label class="top-label">Number:</label>
-                <input class="form-control" type="tel" v-model="sale.number" placeholder="Number"/>
-            </b-col>
-            <b-col cols=2 offset=2>
-                <label class="top-label">Pay Terms:</label>
-                <input class="form-control" type="tel" v-model="sale.paymentTerms" placeholder="Pay Terms"/>
-            </b-col>
-        </b-row>
-        <b-row>
-            <b-col cols=4 style="border: ">
-                <label class="top-label">Vendor:</label><br/>
-                <label class="top-label">Marketplace Brands LLC</label>
-            </b-col>
-            <b-col cols=4>
-                <label class="top-label">shipping to Address:</label>
-                <b-select option-value="id" option-text="dc" :list="customer.addresses" v-model="shippingAddress" placeholder="shipping to address"></b-select>
-            </b-col>
-            <b-col cols=2 offset=2>
-                <label class="top-label">Expected Date:</label>
-                <input class="form-control" type="tel" v-model="sale.expectedDate" placeholder="Date"/>
-            </b-col>
-        </b-row>
-        <b-row>
-            <b-col cols=4 offset=4>
-                <label class="top-label">Street:</label>
-                <input class="form-control" type="tel" readOnly :value="shippingAddress.street" placeholder="City"/>
-            </b-col>
-            <b-col cols=2 offset=2>
-                <label class="top-label">FOB:</label>
-                <input class="form-control" type="tel" v-model="sale.freightTerms" placeholder="Flight Terms"/>
-            </b-col>        
-        <b-row>
-        </b-row>
-            <b-col cols=2 offset=4>
-                <label class="top-label">City:</label>
-                <input class="form-control" type="tel" readOnly :value="shippingAddress.city" placeholder="City"/>
-            </b-col>
-            <b-col cols=1>
-                <label class="top-label">State:</label>
-                <input class="form-control" type="tel" readOnly :value="shippingAddress.state" placeholder=""/>
-            </b-col>
-            <b-col cols=2>
-                <label class="top-label">Zip Code:</label>
-                <input class="form-control" type="tel" readOnly :value="shippingAddress.zip" placeholder="Zip"/>
-            </b-col>
-        </b-row>
-        <b-row>
-            <b-col cols=4>
-                <label class="top-label">Available Items:</label>
-                <b-select option-value="id" option-text="label" :list="availableItems" v-model="item" placeholder="Customer"></b-select>
-            </b-col>
-            <b-col style="padding-top: 30px" cols=1>
-                <b-button variant="link" @click="addItem()">(+)</b-button>
-            </b-col>
-        </b-row>
-        <b-row>
-            <b-col>
-                <label class="top-label"></label>
-                <b-table v-if="sale.saleItems.length>0"
-                    :sort-by.sync="sortBy"
-                    :sort-desc.sync="sortDesc"
-                    :items="sale.saleItems"
-                    :fields="columns">
-                    <template slot="item.number" slot-scope="row">
-                        <b-button size="sm" @click.stop="goToItem(row.item.item.id)" variant="link">{{row.item.item.number}}</b-button>
-                    </template>
-                    <template slot="action" slot-scope="row">
-                        <b-button size="sm" @click.stop="remove(row.item.id)">x</b-button>
-                    </template>
-                </b-table>
-            </b-col>
-        </b-row>
-    </b-container>
+  <b-container fluid>
+    <div style="border: 0px" class="d-flex justify-content-between align-items-center">
+      <h4 style="text-align: left;">Sale Order:</h4>
+      <div style="text-align: right;">
+        <!-- <b-button type="submit" variant="primary" @click="saveSale">Save</b-button> -->
+        <b-button type="reset" variant="success" @click="saveAndClose">Save & Close</b-button>
+      </div>
+    </div>
+    <b-row>
+      <b-col cols="4">
+        <label class="top-label">Customer:</label>
+        <b-select option-value="id" option-text="name" :list="availableCustomers" v-model="customer" placeholder="Customer"></b-select>
+      </b-col>
+      <b-col cols="2">
+        <label class="top-label">Date:</label>
+        <input class="form-control" type="tel" v-model="sale.date" placeholder="Date">
+      </b-col>
+      <b-col cols="2">
+        <label class="top-label">Number:</label>
+        <input class="form-control" type="tel" v-model="sale.number" placeholder="Number">
+      </b-col>
+      <b-col cols="2" offset="2">
+        <label class="top-label">Pay Terms:</label>
+        <input class="form-control" type="tel" v-model="sale.paymentTerms" placeholder="Pay Terms">
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="4" style="border: ">
+        <label class="top-label">Vendor:</label>
+        <br>
+        <label class="top-label">Marketplace Brands LLC</label>
+      </b-col>
+      <b-col cols="4">
+        <label class="top-label">shipping to Address:</label>
+        <b-select option-value="id" option-text="dc" :list="customer.addresses" v-model="shippingAddress" placeholder="shipping to address"></b-select>
+      </b-col>
+      <b-col cols="2" offset="2">
+        <label class="top-label">Expected Date:</label>
+        <input class="form-control" type="tel" v-model="sale.expectedDate" placeholder="Date">
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="4" offset="4">
+        <label class="top-label">Street:</label>
+        <input class="form-control" type="tel" readonly :value="shippingAddress.street" placeholder="City">
+      </b-col>
+      <b-col cols="2" offset="2">
+        <label class="top-label">FOB:</label>
+        <input class="form-control" type="tel" v-model="sale.freightTerms" placeholder="Flight Terms">
+      </b-col>
+      <b-row></b-row>
+      <b-col cols="2" offset="4">
+        <label class="top-label">City:</label>
+        <input class="form-control" type="tel" readonly :value="shippingAddress.city" placeholder="City">
+      </b-col>
+      <b-col cols="1">
+        <label class="top-label">State:</label>
+        <input class="form-control" type="tel" readonly :value="shippingAddress.state" placeholder>
+      </b-col>
+      <b-col cols="2">
+        <label class="top-label">Zip Code:</label>
+        <input class="form-control" type="tel" readonly :value="shippingAddress.zip" placeholder="Zip">
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="4">
+        <label class="top-label">Available Items:</label>
+        <b-select option-value="id" option-text="label" :list="availableItems" v-model="item" placeholder="Customer"></b-select>
+      </b-col>
+      <b-col style="padding-top: 30px" cols="1">
+        <b-button variant="link" @click="addItem()">(+)</b-button>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <label class="top-label"></label>
+        <b-table v-if="sale.saleItems.length>0" :sort-by.sync="sortBy" :sort-desc.sync="sortDesc" :items="sale.saleItems" :fields="columns">
+          <template slot="item.number" slot-scope="row">
+            <b-button size="sm" @click.stop="goToItem(row.item.item.id)" variant="link">{{row.item.item.number}}</b-button>
+          </template>
+          <template slot="action" slot-scope="row">
+            <b-button size="sm" @click.stop="deleteItem(row.item.item.id)">x</b-button>
+          </template>
+        </b-table>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -130,7 +126,7 @@ export default {
     },
     customer: function(new_value, old_value) {
       this.sale.customer = new_value;
-      if(old_value.id && new_value.id != old_value.id){
+      if (old_value.id && new_value.id != old_value.id) {
         this.shippingAddress = {};
       }
     }
@@ -163,9 +159,9 @@ export default {
         });
     },
     saveAndClose() {
-      this.saveSale().then(r=>{
+      this.saveSale().then(r => {
         window.history.back();
-      })
+      });
     },
     getAvailableCustomers() {
       http
@@ -195,10 +191,15 @@ export default {
       if (item) {
         return;
       }
-      this.sale.saleItems.push({ item: this.item});
+      this.sale.saleItems.push({ item: this.item });
     },
     goToItem(item_id) {
       router.push("/itemEdit/" + item_id);
+    },
+    deleteItem(item_id) {
+      var item = this.sale.saleItems.find(it => it.item.id == item_id);
+      this.sale.saleItems.splice(this.sale.saleItems.indexOf(item), 1);
+      this.saveSale();
     }
   },
   mounted() {
