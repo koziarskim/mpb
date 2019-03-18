d828020fa86a13c6be92d1a70529720a027cb719
Marcin Koziarski <koziarskim@yahoo.com>
2019-03-18 17:07:15 -0500

    Add filter to purchase orders.

--- vue/src/components/PurchaseList.vue
+++ vue/src/components/PurchaseList.vue
@@ -1,40 +1,43 @@
 <template>
-    <b-container fluid>
-        <div class="d-flex justify-content-between align-items-center">
-            <h2 style="text-align: left;">Purchase Orders</h2>
-            <div style="text-align: right;">
-                <b-button type="submit" variant="primary" @click="goToPurchase('')">New P.O.</b-button>
-            </div>
+  <b-container fluid>
+    <b-row style="padding-bottom: 4px;">
+      <b-col cols="2">
+        <span style="text-align: left; font-size: 18px; font-weight: bold">Purchase Orders</span>
+      </b-col>
+      <b-col cols="3">
+        <input class="form-control" type="text" v-model="keyword" placeholder="Type to search">
+      </b-col>
+      <b-col>
+        <div style="text-align: right;">
+          <b-button type="submit" variant="primary" @click="goToPurchase('')">New P.O.</b-button>
         </div>
-        <div v-if="purchases.length==0">Not found any purchase orders...</div>
-        <b-table v-if="purchases.length>0"
-                :sort-by.sync="sortBy"
-                :sort-desc.sync="sortDesc"
-                :items="purchases"
-                :fields="fields">
-                <template slot="number" slot-scope="row">
-                    <b-button size="sm" @click.stop=goToPurchase(row.item.id) variant="link">{{row.item.number}}</b-button>
-                </template>
-                <template slot="completed" slot-scope="row">
-                    <span>{{row.item.completed?"Yes":"No"}}</span>
-                </template>
-                <template slot="action" slot-scope="row">
-                    <b-button size="sm" @click.stop="deletePurchase(row.item.id)">x</b-button>
-                </template>
-                <template slot="pdf" slot-scope="row">
-                    <a :href="rowPdfUrl(row.item.id)" target="_blank"><img src="../assets/pdf-download.png" width="20px"/></a>
-                </template>
-                <template slot="expectedDate" slot-scope="row">
-                    <span>{{row.item.expectedDate | formatDate}}</span>
-                </template>
-                <template slot="date" slot-scope="row">
-                    <span>{{row.item.date | formatDate}}</span>
-                </template>
-        </b-table>
-        <b-alert :show="alertSecs" dismissible variant="warning" @dismiss-count-down="(secs) => { alertSecs = secs }">
-                {{alertMessage}}
-        </b-alert>
-    </b-container>
+      </b-col>
+    </b-row>
+    <div v-if="purchases.length==0">Not found any purchase orders...</div>
+    <b-table v-if="purchases.length>0" :sort-by.sync="sortBy" :sort-desc.sync="sortDesc" :items="items" :fields="fields" :keyword="keyword">
+      <template slot="number" slot-scope="row">
+        <b-button size="sm" @click.stop="goToPurchase(row.item.id)" variant="link">{{row.item.number}}</b-button>
+      </template>
+      <template slot="completed" slot-scope="row">
+        <span>{{row.item.completed?"Yes":"No"}}</span>
+      </template>
+      <template slot="action" slot-scope="row">
+        <b-button size="sm" @click.stop="deletePurchase(row.item.id)">x</b-button>
+      </template>
+      <template slot="pdf" slot-scope="row">
+        <a :href="rowPdfUrl(row.item.id)" target="_blank">
+          <img src="../assets/pdf-download.png" width="20px">
+        </a>
+      </template>
+      <template slot="expectedDate" slot-scope="row">
+        <span>{{row.item.expectedDate | formatDate}}</span>
+      </template>
+      <template slot="date" slot-scope="row">
+        <span>{{row.item.date | formatDate}}</span>
+      </template>
+    </b-table>
+    <b-alert :show="alertSecs" dismissible variant="warning" @dismiss-count-down="(secs) => { alertSecs = secs }">{{alertMessage}}</b-alert>
+  </b-container>
 </template>
 <script>
 import http from "../http-common";
@@ -46,7 +49,7 @@ export default {
     return {
       alertSecs: 0,
       alertMessage: "",
-      sortBy: 'id',
+      sortBy: "id",
       sortDesc: false,
       fields: [
         { key: "number", label: "P.O. #", sortable: true },
@@ -55,15 +58,34 @@ export default {
         { key: "expectedDate", label: "Expected", sortable: true },
         { key: "completed", label: "Completed", sortable: true },
         { key: "pdf", label: "PDF", sortable: true },
-        { key: "action", label: "Action", sortable: false}
+        { key: "action", label: "Action", sortable: false }
       ],
-      purchases: []
+      purchases: [],
+      keyword: ""
     };
   },
+  computed: {
+    items() {
+      var filtered = [];
+      if (this.keyword) {
+        this.purchases.filter(item => {
+          if (
+            item.number.includes(this.keyword) ||
+            item.supplier.name.includes(this.keyword) ||
+            (item.date?item.date.includes(this.keyword):false)
+          ) {
+            filtered.push(item);
+          }
+        });
+      } else {
+        filtered = this.purchases;
+      }
+      return filtered;
+    }
+  },
   methods: {
-    showAlert (message) {
-      this.alertSecs = 3,
-      this.alertMessage = message
+    showAlert(message) {
+      (this.alertSecs = 3), (this.alertMessage = message);
     },
     getPurchasesData() {
       http
@@ -73,48 +95,48 @@ export default {
           console.log("Success getting component data");
         })
         .catch(e => {
-          console.log("API error: "+e);
+          console.log("API error: " + e);
         });
     },
-    getItem(component_id){
-        var component;
-        var found = this.purchases.some(function(element) {
-           if(element.id === component_id){
-                component = element;
-           }
-        });
-        return component;
+    getItem(component_id) {
+      var component;
+      var found = this.purchases.some(function(element) {
+        if (element.id === component_id) {
+          component = element;
+        }
+      });
+      return component;
     },
     deletePurchase(id) {
       http
-        .delete("/purchase/"+id)
+        .delete("/purchase/" + id)
         .then(response => {
           this.getPurchasesData();
         })
         .catch(e => {
-            console.log("API Error: "+e);
+          console.log("API Error: " + e);
         });
     },
-    goToPurchase(id){
-        if(!id){
-            http
-            .post("/purchase")
-            .then(response =>{
-                router.push('/purchaseEdit/'+response.data.id);
-            })
-            .catch(e =>{
-                console.log("API Error: "+e);
-            })
-        }else{
-            router.push('/purchaseEdit/'+id);
-        }
+    goToPurchase(id) {
+      if (!id) {
+        http
+          .post("/purchase")
+          .then(response => {
+            router.push("/purchaseEdit/" + response.data.id);
+          })
+          .catch(e => {
+            console.log("API Error: " + e);
+          });
+      } else {
+        router.push("/purchaseEdit/" + id);
+      }
     },
     rowPdfUrl: function(purchase_id) {
       return httpUtils.baseUrl + "/purchase/" + purchase_id + "/pdf";
     }
   },
   mounted() {
-     this.getPurchasesData();
+    this.getPurchasesData();
   }
 };
 </script>
