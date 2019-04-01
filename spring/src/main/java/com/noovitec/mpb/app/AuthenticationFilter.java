1aec5ddbc54b1f6693e693485d4e7501f335954a
Marcin Koziarski <koziarskim@yahoo.com>
2019-04-01 18:21:33 -0500

    Clean up.

--- spring/src/main/java/com/noovitec/mpb/app/AuthenticationFilter.java
+++ spring/src/main/java/com/noovitec/mpb/app/AuthenticationFilter.java
@@ -16,7 +16,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
--- spring/src/main/java/com/noovitec/mpb/app/MpbConfiguration.java
+++ spring/src/main/java/com/noovitec/mpb/app/MpbConfiguration.java
@@ -1,14 +1,13 @@
 package com.noovitec.mpb.app;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@ComponentScan({ "com.noovitec.mpb.rest", "com.noovitec.mpb.service", "com.noovitec.mpb.app" })
+@ComponentScan({ "com.noovitec.mpb.rest", "com.noovitec.mpb.service" })
 @EntityScan("com.noovitec.mpb.entity")
 @EnableJpaRepositories("com.noovitec.mpb.repo")
 @Configuration
@@ -20,10 +19,4 @@ public class MpbConfiguration implements WebMvcConfigurer {
 		registry.addInterceptor(apiInterceptor);
 		WebMvcConfigurer.super.addInterceptors(registry);
 	}
-
-//	@Override
-//	public void addCorsMappings(CorsRegistry registry) {
-//		registry.addMapping("/**").allowedOrigins("*").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH").allowCredentials(true)
-//		.allowedHeaders(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN);
-//	}
 }
