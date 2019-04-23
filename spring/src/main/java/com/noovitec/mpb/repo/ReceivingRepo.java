8b776c6f9198bb361bc5b1a07451cf7b160e6501
Marcin Koziarski <koziarskim@yahoo.com>
2019-04-23 10:14:44 -0500

    Fix query.

--- spring/src/main/java/com/noovitec/mpb/repo/ReceivingRepo.java
+++ spring/src/main/java/com/noovitec/mpb/repo/ReceivingRepo.java
@@ -16,16 +16,15 @@ public interface ReceivingRepo extends JpaRepository<Receiving, Long> {
 	/*
 		select pc.component_id, sum(r.units) from receiving r
 		join purchase_component pc on pc.id = r.purchase_component_id
-		join item_component ic on ic.component_id = pc.component_id
 		where r.eta_date <= '2019-05-07 19:00:00'
 		and r.received_date is null
 		group by pc.component_id
 	 */
-	@Query("select new com.noovitec.mpb.dto.KeyValueDto(ic.component.id, sum(r.units)) "
+	@Query("select new com.noovitec.mpb.dto.KeyValueDto(pc.component.id, sum(r.units)) "
 			+ "from Receiving r "
-			+ "join r.purchaseComponent.component.itemComponents ic "
+			+ "join r.purchaseComponent pc "
 			+ "where r.etaDate <= :date and r.receivedDate is null "
-			+ "group by ic.component.id")
+			+ "group by pc.component.id")
 	List<KeyValueDto> findReceivingByEta(@Param("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
 
 }
