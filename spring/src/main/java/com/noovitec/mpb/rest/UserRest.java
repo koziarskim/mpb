24f62d5a30fc868fb770cf9b588794f8ab1867bf
Marcin Koziarski <koziarskim@yahoo.com>
2019-05-14 10:36:30 -0500

    Set login timeout for 15 mins.

--- spring/src/main/java/com/noovitec/mpb/rest/UserRest.java
+++ spring/src/main/java/com/noovitec/mpb/rest/UserRest.java
@@ -77,6 +77,7 @@ class UserRest {
 		mpbAuthenticationContext.addSid(sid);
 		Cookie cookie = new Cookie("SID", sid);
 		cookie.setPath("/");
+		cookie.setMaxAge(900); //15 mins.
 		response.addCookie(cookie);
 		return ResponseEntity.ok().body(result);
 	}
