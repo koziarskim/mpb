bcc9a8b9f011e3e3494769990fdcd0401005112c
Marcin Koziarski <koziarskim@yahoo.com>
2019-08-30 11:46:29 -0500

    Add line and item filter on ProductionLineList

--- vue/src/components/ProductionLineList.vue
+++ vue/src/components/ProductionLineList.vue
@@ -1,15 +1,21 @@
 <template>
   <b-container fluid>
     <b-row style="padding-bottom: 4px;">
-      <b-col cols="3">
+      <b-col cols=3>
         <span style="font-size: 18px; font-weight: bold">Daily Production Status:</span>
       </b-col>
-      <b-col cols="2">
+      <b-col cols=2>
         <input class="form-control" type="date" v-model="date" placeholder="Date">
       </b-col>
+      <b-col cols=1>
+        <b-select option-value="id" option-text="number" :list="availableLines" v-model="selectedLine"></b-select>
+      </b-col>
+      <b-col cols=3>
+        <b-select option-value="id" option-text="name" :list="availableItems" v-model="selectedItem"></b-select>
+      </b-col>
     </b-row>
-    <div v-if="schedule.scheduleEvents.length==0">No lines set for this date</div>
-    <b-table v-if="schedule.scheduleEvents.length>0" :sort-by.sync="sortBy" :sort-desc.sync="sortDesc" :items="schedule.scheduleEvents" :fields="fields">
+    <div v-if="scheduleEvents.length==0">No lines set for this date</div>
+    <b-table v-if="scheduleEvents.length>0" :sort-by.sync="sortBy" :sort-desc.sync="sortDesc" :items="scheduleEvents" :fields="fields">
       <template slot="line.number" slot-scope="row">
         <b-button size="sm" @click.stop="goToProductionLine(row.item.id)" variant="link">{{row.item.line.number}}</b-button>
       </template>
@@ -38,38 +44,77 @@ export default {
       sortDesc: false,
       fields: [
         { key: "line.number", label: "Line", sortable: true },
-		{ key: "item", label: "Item (Sale & Customer)", sortable: true },
-		{ key: "unitsScheduled", label: "Units Scheduled", sortable: true },
-		{ key: "totalProduced", label: "Total Produced", sortable: true },
+		    { key: "item", label: "Item (Sale & Customer)", sortable: true },
+		    { key: "unitsScheduled", label: "Units Scheduled", sortable: true },
+		    { key: "totalProduced", label: "Total Produced", sortable: true },
         { key: "unitsPending", label: "Still To Make", sortable: true },
-		{ key: "totalTime", label: "Total Time", sortable: true },
+		    { key: "totalTime", label: "Total Time", sortable: true },
 	  ],
-	  schedule: {
-		  scheduleEvents: []
-	  }
+    scheduleEvents: [],
+    availableLines: [],
+    availableItems: [],
+    selectedLine: {},
+    selectedItem: {}
     };
   },
   watch: {
     date(newValue, oldValue) {
-      this.getSchedule(newValue);
+      this.getScheduleEvents(newValue);
+    },
+    selectedLine(newValue, oldValue){
+      this.getScheduleEvents(this.date);
+    },
+    selectedItem(newValue, oldValue){
+      this.getScheduleEvents(this.date);
     }
   },
   methods: {
+  getAvailableLines() {
+		this.availableLines = [
+			{ id: 1, number: 1 },
+			{ id: 2, number: 2 },
+			{ id: 3, number: 3 },
+			{ id: 4, number: 4 },
+			{ id: 5, number: 5 },
+			{ id: 6, number: 6 },
+			{ id: 7, number: 7 },
+			{ id: 8, number: 8 }
+		];
+  },
+  getAvailableItems(){
+    if(this.selectedItem.id){
+      return;
+    }
+    this.availableItems = [];
+    this.scheduleEvents.forEach(event => {
+      this.availableItems.push({
+        id: event.saleItem.item.id,
+        name: event.saleItem.item.name
+      })
+    })
+  },
 	formatTime(secs){
 		const duration = moment.duration(secs, 'seconds');
 		return duration.hours()+':'+duration.minutes()+':'+duration.seconds();
 	},
-	getSchedule(date){
+	getScheduleEvents(date){
       http
         .get("/schedule/single/date/"+date)
         .then(response => {
-			if(response.data){
-		  		this.schedule = response.data;
-			}else{
-				this.schedule = {
-					scheduleEvents: []
-				}
-			}
+          this.scheduleEvents = [];
+          if(response.data){
+            response.data.scheduleEvents.forEach(event =>{
+              console.log(this.selectedLine.id)
+              if(this.selectedLine.id && event.line.id != this.selectedLine.id){
+                return;
+              }
+              if(this.selectedItem.id && event.saleItem.item.id != this.selectedItem.id){
+                return;
+              }
+              this.scheduleEvents.push(event)
+            })
+          }
+          this.getAvailableItems();
         })
         .catch(e => {
           console.log("API error: " + e);
@@ -84,7 +129,8 @@ export default {
 	},
   },
   mounted() {
-	this.getSchedule(this.date);
+  this.getScheduleEvents(this.date);
+  this.getAvailableLines();
   }
 };
 </script>
