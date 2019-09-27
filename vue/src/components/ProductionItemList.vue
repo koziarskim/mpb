44f633d93220bf3cf2ca773ab9301d81205ec029
Marcin Koziarski <koziarskim@yahoo.com>
2019-09-27 11:15:43 -0500

    Production item list (percentages).

--- vue/src/components/ProductionItemList.vue
+++ vue/src/components/ProductionItemList.vue
@@ -11,43 +11,44 @@
     </b-row>
     <br/>
     <b-row style="font-weight:bold; background-color:lightgray">
-      <b-col cols=5>Item<br/> (Sale - Customer)</b-col>
+      <b-col cols=4>Item<br/> (Sale - Customer)</b-col>
       <b-col cols=1>Total<br/>Sold</b-col>
       <b-col cols=1>Total<br/>Produced</b-col>
-      <b-col cols=1>Daily<br/>Scheduled</b-col>
-      <b-col cols=1>Daily<br/>Produced</b-col>
+      <b-col cols=1>Total %<br/>Produced</b-col>
+      <b-col cols=1>Daily [u]<br/>Sched/Prod</b-col>
+      <b-col cols=1>Daily %<br/>Produced</b-col>
       <b-col cols=1>Daily<br/>Duration</b-col>
-      <b-col cols=1>T / D<br/>Average</b-col>
-      <b-col cols=1>Daily<br/>Progress</b-col>
+      <b-col cols=1>Perf. [u/h]<br/>Total/Daily</b-col>
+      <b-col cols=1>Perf. %<br/>Total/Daily</b-col>
     </b-row>
     <div v-for="item in items" v-bind:key="item.id">
       <b-row>
-        <b-col cols=5>
+        <b-col cols=4 style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
           <b-button size="sm" @click="toggleRow(item)" variant="link">{{item.show?'[-]':'[+]'}}</b-button>{{item.name}}
         </b-col>
         <b-col cols=1>{{item.totalSold}}</b-col>
         <b-col cols=1>
           <a href="#" @click="goToProductionLine(item.id)">{{item.totalProduced}}</a>
         </b-col>
-        <b-col cols=1>{{item.dailyScheduled}}</b-col>
-        <b-col cols=1>{{item.dailyProduced}}</b-col>
+        <b-col cols=1>{{percOut(item.totalProduced,item.totalSold)}}</b-col>
+        <b-col cols=1>{{item.dailyScheduled+"/"+item.dailyProduced}}</b-col>
+        <b-col cols=1>{{percOut(item.dailyProduced,item.dailyScheduled)}}</b-col>
         <b-col cols=1>{{formatter.secondsToTime(item.dailySeconds)}}</b-col>
         <b-col cols=1>{{item.totalAverage+"/"+item.dailyAverage}}</b-col>
-        <b-col cols=1>{{getPerc(item.dailyAverage,item.totalAverage)}}</b-col>
+        <b-col cols=1>{{percPerf(item.dailyAverage,item.totalAverage)}}</b-col>
       </b-row>
         <div v-for="event in item.events" v-bind:key="event.id">
           <div v-if="item.show">
           <b-row style="color: gray; font-style: italic">
-            <b-col cols=5><div style="padding-left:50px">{{"Line "+event.lineNumber+": ("+event.saleNumber + " - " + event.customerName+")"}}</div></b-col>
+            <b-col cols=4><div style="padding-left:50px">{{"Line "+event.lineNumber+": ("+event.saleNumber + " - " + event.customerName+")"}}</div></b-col>
             <b-col cols=1>{{event.unitsSold}}</b-col>
             <b-col cols=1>{{event.saleTotalProduced}}</b-col>
-            <b-col cols=1>{{event.dailyScheduled}}</b-col>
-            <b-col cols=1>
-              <a href="#" @click="goToProductionLine(event.id)">{{event.dailyProduced}}</a>
-            </b-col>
+            <b-col cols=1>{{percOut(event.saleTotalProduced,event.unitsSold)}}</b-col>
+            <b-col cols=1><a href="#" @click="goToProductionLine(event.id)">{{event.dailyScheduled+"/"+event.dailyProduced}}</a></b-col>
+            <b-col cols=1>{{percOut(event.dailyProduced,event.dailyScheduled)}}</b-col>
             <b-col cols=1>{{formatter.secondsToTime(event.dailySeconds)}}</b-col>
-            <b-col cols=1>{{event.dailyAverage}}</b-col>
-            <b-col cols=1>{{getPerc(event.dailyAverage,item.totalAverage)}}</b-col>
+            <b-col cols=1>{{item.totalAverage+"/"+event.dailyAverage}}</b-col>
+            <b-col cols=1>{{percPerf(event.dailyAverage,item.totalAverage)}}</b-col>
           </b-row>
         </div>
       </div>
@@ -227,12 +228,16 @@ export default {
     goToProductionLine(schedule_event_id) {
       router.push("/productionLine/" + schedule_event_id);
     },
-    getPerc(high, low){
+    percPerf(high, low){
       if(high==0 || low==0){
         return "0%"
       }
       return (((high - low) / ((high + low)/2)) * 100).toFixed(0)+"%";
-}
+    },
+    percOut(high, low){
+      return ((high / low) * 100).toFixed(0)+"%";
+    }
+
   },
   mounted() {
     var date = this.$route.params.date;
