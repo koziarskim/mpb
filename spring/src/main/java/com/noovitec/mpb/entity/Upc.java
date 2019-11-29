3ff69aabda2505da1e0cacbe665feafc76651891
Marcin Koziarski <koziarskim@yahoo.com>
2019-11-29 10:59:38 -0600

    Remove unused association from Upc

--- spring/src/main/java/com/noovitec/mpb/entity/Upc.java
+++ spring/src/main/java/com/noovitec/mpb/entity/Upc.java
@@ -1,11 +1,6 @@
 package com.noovitec.mpb.entity;
 
-import java.util.Collection;
-
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,7 +17,4 @@ public class Upc extends BaseEntity {
 	private String code;
 	private boolean assigned;
 
-	@OneToMany(fetch = FetchType.LAZY)
-	@JoinColumn(name = "upc_id")
-	private Collection<Item> items;
 }
