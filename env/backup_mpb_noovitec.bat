f7fdf7e2755ce77c95656b28240cb0fa87a8df3d
Marcin Koziarski <koziarskim@yahoo.com>
2019-12-19 14:46:51 -0600

    Update backup DB so it includes import.

--- env/backup_mpb_noovitec.bat
+++ env/backup_mpb_noovitec.bat
@@ -1,31 +1,31 @@
 @echo off
 rem Make sure to set PGPASSWORD environment variable.  Just like "JAVA_HOME".
 
-for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
- set dow=%%i
- set month=%%j
- set day=%%k
- set year=%%l
+for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (set datestr=%%l_%%j_%%k)
+rem for /f "tokens=1-4 delims=/:." %%a in ("%TIME%") do (set timestr=%%a_%%b_%%c)
+
+set ARG=%1%
+set DATE_FORMAT=%datestr%
+set BACKUP_DIR=C:\Users\kozia\postgres\backups\noovitec
+set SCHEMA_FILE=%BACKUP_DIR%\backup_schema_%DATE_FORMAT%.sql
+set DATA_FILE=%BACKUP_DIR%\backup_data_%DATE_FORMAT%.sql
+set DB_NAME=mpb_%DATE_FORMAT%
+echo Schema file name is %SCHEMA_FILE%
+echo Data file name is %DATA_FILE%
+echo DB name: %DB_NAME%
+echo Processing: %ARG%
+
+if "%ARG:~0,6%"=="backup" (
+	if exist %SCHEMA_FILE% (echo Schema File already exist %SCHEMA_FILE% && GOTO import)
+	"C:\Program Files (x86)\pgAdmin 4\v4\runtime\pg_dump.exe" --file %SCHEMA_FILE% --host "mpb.noovitec.com" --port "5432" --username "postgres" --verbose --format=p --create --inserts --column-inserts --schema-only "mpb"
+	"C:\Program Files (x86)\pgAdmin 4\v4\runtime\pg_dump.exe" --file %DATA_FILE% --host "mpb.noovitec.com" --port "5432" --username "postgres" --verbose --format=p --create --inserts --column-inserts --data-only "mpb"
 )
-set datestr=%month%_%day%_%year%
-echo datestr is %datestr%
-
-set SCHEMA_FILE=C:\Users\kozia\postgres\backups\noovitec\backup_schema_%datestr%.sql
-set DATA_FILE=C:\Users\kozia\postgres\backups\noovitec\backup_data_%datestr%.sql
-echo backup schema file name is %SCHEMA_FILE%
-echo backup data file name is %DATA_FILE%
-echo on
-
-rem "C:\Program Files (x86)\pgAdmin 4\v4\runtime\pg_dump.exe" --file %SCHEMA_FILE% --host "mpb.noovitec.com" --port "5432" --username "postgres" --verbose --format=p --create --inserts --column-inserts --schema-only "mpb"
-
-rem "C:\Program Files (x86)\pgAdmin 4\v4\runtime\pg_dump.exe" --file %DATA_FILE% --host "mpb.noovitec.com" --port "5432" --username "postgres" --verbose --format=p --create --inserts --column-inserts --data-only "mpb"
-
-echo off
-findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %SCHEMA_FILE% > clean_schema.sql
-findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %DATA_FILE% > clean_data.sql
-
-createdb -h localhost -p 5432 -U postgres mpb
-
-psql -U postgres -d mpb < clean_schema.sql
-
-psql -U postgres -d mpb < clean_data.sql
+:import
+if "%ARG:~6,12%"=="import" (
+	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %SCHEMA_FILE% > %BACKUP_DIR%\clean_schema.sql
+	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %DATA_FILE% > %BACKUP_DIR%\clean_data.sql
+	createdb -h localhost -p 5432 -U postgres %DB_NAME% || (GOTO end)
+	psql -U postgres -d %DB_NAME% < %BACKUP_DIR%\clean_schema.sql
+	psql -U postgres -d %DB_NAME% < %BACKUP_DIR%\clean_data.sql
+)
+:end
