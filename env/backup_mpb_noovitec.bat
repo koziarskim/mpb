d368e00c83266ae90c0b52e8437b7d9a81765b19
Marcin Koziarski <koziarskim@yahoo.com>
2019-12-20 11:06:17 -0600

    update backup script.

--- env/backup_mpb_noovitec.bat
+++ env/backup_mpb_noovitec.bat
@@ -5,6 +5,8 @@ for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (set datestr=%%l_%%j_%%k)
 rem for /f "tokens=1-4 delims=/:." %%a in ("%TIME%") do (set timestr=%%a_%%b_%%c)
 
 set ARG=%1%
+set BACKUP=false
+set IMPORT=false
 set DATE_FORMAT=%datestr%
 set BACKUP_DIR=C:\Users\kozia\postgres\backups\noovitec
 set SCHEMA_FILE=%BACKUP_DIR%\backup_schema_%DATE_FORMAT%.sql
@@ -15,13 +17,24 @@ echo Data file name is %DATA_FILE%
 echo DB name: %DB_NAME%
 echo Processing: %ARG%
 
-if "%ARG:~0,6%"=="backup" (
+if "%ARG%"=="backup" (
+	set BACKUP=true
+)
+if "%ARG%"=="import" (
+	set IMPORT=true
+)
+if "%ARG%"=="full" (
+	set BACKUP=true
+	set IMPORT=true
+)
+:backup
+if "%BACKUP%" == "true" (
 	if exist %SCHEMA_FILE% (echo Schema File already exist %SCHEMA_FILE% && GOTO import)
 	"C:\Program Files (x86)\pgAdmin 4\v4\runtime\pg_dump.exe" --file %SCHEMA_FILE% --host "mpb.noovitec.com" --port "5432" --username "postgres" --verbose --format=p --create --inserts --column-inserts --schema-only "mpb"
-	"C:\Program Files (x86)\pgAdmin 4\v4\runtime\pg_dump.exe" --file %DATA_FILE% --host "mpb.noovitec.com" --port "5432" --username "postgres" --verbose --format=p --create --inserts --column-inserts --data-only "mpb"
+	"C:\Program Files (x86)\pgAdmin 4\v4\runtime\pg_dump.exe" --file %DATA_FILE% --host "mpb.noovitec.com" --port "5432" --username "postgres" --verbose --format=p --create --inserts --column-inserts --data-only "mpb".
 )
 :import
-if "%ARG:~6,12%"=="import" (
+if "%IMPORT%" == "true" (
 	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %SCHEMA_FILE% > %BACKUP_DIR%\clean_schema.sql
 	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %DATA_FILE% > %BACKUP_DIR%\clean_data.sql
 	createdb -h localhost -p 5432 -U postgres %DB_NAME% || (GOTO end)
