ae4c4994b8c8b9481578195108e5b1252c4320fb
Marcin Koziarski <koziarskim@yahoo.com>
2020-01-23 07:32:55 -0600

    status migration.

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -1,1 +1,8 @@
 alter table shipment add column status char(3);
+
+update shipment set status = 'INP' where status is null;
+
+update shipment set status = 'REA' where ready is true;
+
+update shipment set status = 'SHP' where shipped_date is not null;
+
