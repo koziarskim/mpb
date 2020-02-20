505178f8b9df154e067b7def942ff5fb86615bfa
Marcin Koziarski <koziarskim@yahoo.com>
2020-02-19 22:01:27 -0600

    Mail using velocity.

--- spring/src/main/java/com/noovitec/mpb/app/MpbConfiguration.java
+++ spring/src/main/java/com/noovitec/mpb/app/MpbConfiguration.java
@@ -1,10 +1,14 @@
 package com.noovitec.mpb.app;
 
+import java.io.IOException;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.VelocityException;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,5 +23,14 @@ public class MpbConfiguration implements WebMvcConfigurer {
 		ApiInterceptor apiInterceptor = new ApiInterceptor();
 		registry.addInterceptor(apiInterceptor);
 		WebMvcConfigurer.super.addInterceptors(registry);
+	}
+
+	@Bean
+	public VelocityEngine getVelocityEngine() throws VelocityException, IOException {
+		VelocityEngine velocityEngine = new VelocityEngine();
+		velocityEngine.setProperty("resource.loader", "class");
+		velocityEngine.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+		velocityEngine.init();
+		return velocityEngine;
 	}
 }
--- spring/src/main/java/com/noovitec/mpb/rest/MailRest.java
+++ spring/src/main/java/com/noovitec/mpb/rest/MailRest.java
@@ -1,26 +1,30 @@
 package com.noovitec.mpb.rest;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.UUID;
 
 import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.servlet.http.HttpServletRequest;
 
+import org.apache.velocity.app.VelocityEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,30 +42,52 @@ import com.google.api.services.gmail.model.Message;
 @RequestMapping("/api")
 class MailRest {
 
+	@Autowired
+	VelocityEngine velocityEngine;
+	
 	private final Logger log = LoggerFactory.getLogger(MailRest.class);
 	private static final List<String> SCOPES = Arrays.asList(GmailScopes.GMAIL_SEND, GmailScopes.GMAIL_LABELS);
-	private static final String USCG_JSON_SERVICE_CREDENTIAL_FILE_PATH = "oauth/oauth-project-268605-d9f44c2e6932.json";
-	private static final String USCG_P12_SERVICE_CREDENTIAL_FILE_PATH = "oauth/mims-268617-499ba269d364.p12";
-	private static final String MIMS_JSON_KEY = "oauth/mims-268617-0ecb1dec6a2e.json";
-
-	@Autowired
-	HttpServletRequest request;
+	private static final String MIMS_JSON_KEY = "oauth/mims-268617-f7755598ac50.json";
 
 	@PostMapping("/mail")
 	ResponseEntity<?> post() throws URISyntaxException, GeneralSecurityException, IOException, MessagingException {
+		String notificationId = UUID.randomUUID().toString();
+		Map<String, String> model = new HashMap<String, String>();
+        model.put("name", "Marcin");
+        model.put("notificationId", notificationId);
+        String subject = "MIMS Notification (Test)";
+		String body = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, "mail/genericTemplate.vm", model);
+		InternetAddress[] bcc = {
+				new InternetAddress("mkoziarski@marketplacebrands.com"),
+				new InternetAddress("koziarskim@yahoo.com"),
+//				new InternetAddress("akoziarski@marketplacebrands.com"),
+//				new InternetAddress("hpyzikiewicz@marketplacebrands.com")
+		};
+		this.sendEmail(bcc, subject, body);
+		return ResponseEntity.ok().build();
+	}
+
+	private void sendEmail(InternetAddress[] bcc, String subject, String body) throws GeneralSecurityException, IOException, AddressException, MessagingException {
 		HttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
 		JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-		GoogleCredential credential = this.getJsonCredentials(HTTP_TRANSPORT, JSON_FACTORY);
-		Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("whatever").build();
-		InternetAddress to = new InternetAddress("koziarskim@yahoo.com", "M K");
-//		InternetAddress from = new InternetAddress("m@m.com", "Some Name");
+		InputStream credentialsJSON = this.getClass().getClassLoader().getResourceAsStream(MIMS_JSON_KEY);
+		GoogleCredential gcFromJson = GoogleCredential.fromStream(credentialsJSON, HTTP_TRANSPORT, JSON_FACTORY).createScoped(SCOPES);
+		GoogleCredential credential = new GoogleCredential.Builder()
+				.setTransport(gcFromJson.getTransport())
+				.setJsonFactory(gcFromJson.getJsonFactory())
+				.setServiceAccountId(gcFromJson.getServiceAccountId())
+				.setServiceAccountUser("mims@marketplacebrands.com")
+				.setServiceAccountPrivateKey(gcFromJson.getServiceAccountPrivateKey())
+				.setServiceAccountScopes(gcFromJson.getServiceAccountScopes())
+				.build();
+		Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("MIMS").build();
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 		Session session = Session.getDefaultInstance(new Properties());
 		MimeMessage email = new MimeMessage(session);
-//		email.setFrom(from);
-		email.addRecipient(javax.mail.Message.RecipientType.TO, to);
-		email.setSubject("Subject here");
-		email.setText("body here");
-		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress("mims@marketplacebrands.com"));
+		email.addRecipients(javax.mail.Message.RecipientType.BCC, bcc);
+		email.setSubject(subject);
+		email.setText(body, "UTF-8");
 		email.writeTo(buffer);
 		byte[] bytes = buffer.toByteArray();
 		String encodedEmail = Base64.getUrlEncoder().encodeToString(bytes);
@@ -69,39 +95,6 @@ class MailRest {
 		message.setRaw(encodedEmail);
 		message = service.users().messages().send("me", message).execute();
 		System.out.println("Message id: " + message.getId());
-		System.out.println(message.toPrettyString());
-		return ResponseEntity.ok().build();
-	}
-
-	private GoogleCredential getJsonCredentials(HttpTransport HTTP_TRANSPORT, JsonFactory JSON_FACTORY) throws GeneralSecurityException, IOException {
-		InputStream credentialsJSON = this.getClass().getClassLoader().getResourceAsStream(USCG_JSON_SERVICE_CREDENTIAL_FILE_PATH);
-//		InputStream credentialsJSON = this.getClass().getClassLoader().getResourceAsStream(MIMS_JSON_KEY);
-		GoogleCredential gcFromJson = GoogleCredential.fromStream(credentialsJSON, HTTP_TRANSPORT, JSON_FACTORY).createScoped(SCOPES);
-		return new GoogleCredential.Builder()
-				.setTransport(gcFromJson.getTransport())
-				.setJsonFactory(gcFromJson.getJsonFactory())
-				.setServiceAccountId(gcFromJson.getServiceAccountId())
-				.setServiceAccountUser("mims@uscgcaptains.com")
-//				.setServiceAccountUser("mims@marketplacebrands.com")
-				.setServiceAccountPrivateKey(gcFromJson.getServiceAccountPrivateKey())
-				.setServiceAccountScopes(gcFromJson.getServiceAccountScopes())
-				.build();
-	}
-
-	private GoogleCredential getP12Credentials(HttpTransport HTTP_TRANSPORT, JsonFactory JSON_FACTORY) throws GeneralSecurityException, IOException {
-		// Use the client ID when making the OAuth 2.0 access token request (see
-		// Google's OAuth 2.0 Service Account documentation).
-		String serviceAccountClientID = "105603679281239253431";
-		// Use the email address when granting the service account access to supported
-		// Google APIs
-		String serviceAccountUserEmail = "mims@marketplacebrands.com";
-		GoogleCredential credential = new GoogleCredential.Builder().setTransport(HTTP_TRANSPORT).setJsonFactory(JSON_FACTORY)
-				.setServiceAccountId(serviceAccountClientID) // requesting the token
-				.setServiceAccountPrivateKeyFromP12File(new File(USCG_P12_SERVICE_CREDENTIAL_FILE_PATH)).setServiceAccountScopes(SCOPES) // see
-																																			// https://developers.google.com/gmail/api/auth/scopes
-				.setServiceAccountUser(serviceAccountUserEmail).build();
-		credential.refreshToken();
-		return credential;
 	}
 
 }
