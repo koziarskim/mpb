c273fd3f8b5291feb2b323dfdc0ad56d94dd0ea5
Marcin Koziarski <koziarskim@yahoo.com>
2020-03-12 18:50:38 -0500

    Deployed.

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -1,5 +1,1 @@
-update role set code='STANDARD_ADMIN', name='Standard Admin', description='Allow full access if no other admin' where id=3;
-
-update role set description='Not used' where id=6;
-
-alter table component drop column units_in_transit;
+Deployed.
