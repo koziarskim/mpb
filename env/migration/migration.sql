7a9f1ffc08cd7c2c22ffeb6fd9727d86a2af426a
Marcin Koziarski <koziarskim@yahoo.com>
2020-03-17 19:26:00 -0500

    deployed.

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -1,15 +1,1 @@
-alter table sale add column modified_date timestamp without time zone;
-
-update sale set modified_date = updated where modified_date is null;
-
-alter table sale add column shipping_from date;
-
-alter table sale add column shipping_to date;
-
-update sale set status = 'PENDING_SHIPMENT' where status = 'PENDING_SHIPPMENT';
-
-update sale_item set status = 'PENDING_SHIPMENT' where status = 'PENDING_SHIPPMENT';
-
-update item set upc_id = null;
-
-
+deployed.
