a737adc73cfb5862da05c06ebd4cb26f1d81e383
Marcin Koziarski <koziarskim@yahoo.com>
2020-04-08 21:02:01 -0500

    Enable notifications.

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -1,7 +1,1 @@
-set schema 'y2020';
-
-alter table sale add column pending_approval boolean;
-
-update sale set pending_approval = false;
-
-ALTER TABLE invoice DROP CONSTRAINT uq_invoice_number;
+deployed.
