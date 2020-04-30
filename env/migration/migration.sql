52a4f1647ebb4852f2ad82a36c59774da7fb13e5
Marcin Koziarski <koziarskim@yahoo.com>
2020-04-30 18:48:04 -0500

    Update status. Deployed.

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -1,5 +1,1 @@
-set schema 'y2020';
-alter table receiving add column unit_price numeric(19,4);
-alter table component add column average_price numeric(19,4);
-update component set average_price = 0;
-update receiving set unit_price = 0;
+Deployed.
