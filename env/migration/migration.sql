fc33ce3df93a643844bd78a1fbaa73dfb2f4717d
Marcin Koziarski <koziarskim@yahoo.com>
2020-05-13 08:57:21 -0500

    Deployed.

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -5,8 +5,3 @@ Deployed.
 --End SHARED
 set schema 'y2019';
 
-alter table purchase add column confirmed boolean default false;
-alter table purchase add column notes varchar(255);
-alter table component add column case_weight bigint default 0;
-alter table component add column shelf varchar(255);
-alter table component add column expiration date;
