114af135c8f5c5bdff3432b6066337e946c2d0bf
Marcin Koziarski <koziarskim@yahoo.com>
2020-05-22 07:54:54 -0500

    Fix customer.

--- spring/src/main/java/com/noovitec/mpb/app/MpbEventListener.java
+++ spring/src/main/java/com/noovitec/mpb/app/MpbEventListener.java
@@ -131,15 +131,15 @@ public class MpbEventListener implements PostInsertEventListener, PostUpdateEven
 	    			.build();
 	    	MpbTenantContext.addMessage(message);
 		}
-		if (entity.getClass() == Customer.class) {
-	    	JmsCustomerMessage message = JmsCustomerMessage.builder().id((Long) event.getId())
-	    			.oldUnitsShipped(jmsUtil.getLong("unitsShipped", keys, oldState))
-	    			.unitsShipped(jmsUtil.getLong("unitsShipped", keys, state))
-	    			.unitsSold(jmsUtil.getLong("unitsSold", keys, state))
-	    			.type("customerUpdated")
-	    			.build();
-	    	MpbTenantContext.addMessage(message);
-		}
+//		if (entity.getClass() == Customer.class) {
+//	    	JmsCustomerMessage message = JmsCustomerMessage.builder().id((Long) event.getId())
+//	    			.oldUnitsShipped(jmsUtil.getLong("unitsShipped", keys, oldState))
+//	    			.unitsShipped(jmsUtil.getLong("unitsShipped", keys, state))
+//	    			.unitsSold(jmsUtil.getLong("unitsSold", keys, state))
+//	    			.type("customerUpdated")
+//	    			.build();
+//	    	MpbTenantContext.addMessage(message);
+//		}
 	}
 
 	@Override
