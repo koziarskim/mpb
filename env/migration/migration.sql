26afe237987b8b5b819fc2bc51898dbec2668763
Marcin Koziarski <koziarskim@yahoo.com>
2020-07-30 11:04:50 -0500

    Deploy.

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -5,7 +5,4 @@ set schema 'shared';
 --End SHARED
 set schema 'y2019';
 
-alter table customer add column invoice_email varchar(255);
-
-alter table invoice rename column ap_email to invoice_email;
 
