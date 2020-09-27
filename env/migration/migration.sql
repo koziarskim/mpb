cb7184d468c626161105e9fbc014cc9d00dfa147
Marcin Koziarski <koziarskim@yahoo.com>
2020-09-27 18:24:50 -0500

    Deployed.

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -5,10 +5,5 @@ set schema 'shared';
 --End SHARED
 set schema 'y2019';
 
-alter table sale add column paid_in_full boolean default false;
-
-alter table item add column units_open_sale bigint default 0;
-
-delete from item_component where component_id is null or item_id is null;
 
 
