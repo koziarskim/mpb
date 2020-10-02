46c3fcc77659c791199c1c58ba101a8f452c6f09
Marcin Koziarski <koziarskim@yahoo.com>
2020-10-02 04:39:20 -0500

    Deploy

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -5,7 +5,3 @@ set schema 'shared';
 --End SHARED
 set schema 'y2019';
 
-alter table sale_item add column invoiced_amount numeric(19,2) default 0;
-alter table sale add column invoiced_amount numeric(19,2) default 0;
-
-
