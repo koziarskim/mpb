fc7116858d6b6df85b6581ccf0279caa10bbdf9d
Marcin Koziarski <koziarskim@yahoo.com>
2020-10-27 15:16:45 -0500

    sql units assigned.

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -10,3 +10,5 @@ ALTER TABLE schedule_event ADD CONSTRAINT fk_schedule_event_item_id FOREIGN KEY 
 
 alter table item drop column labor_cost;
 alter table item drop column other_cost;
+
+alter table sale_item add column units_assigned bigint default 0;
