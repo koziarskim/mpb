409e6c1dd8d9cd80cdc1a3847e29e47a096190e9
Marcin Koziarski <koziarskim@yahoo.com>
2020-11-11 09:01:42 -0600

    Deployed.

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -5,14 +5,6 @@ set schema 'shared';
 --End SHARED
 set schema 'y2020';
 
-alter table item_packaging add column units_produced bigint default 0;
-alter table item_packaging add column units_assigned bigint default 0;
-alter table item_packaging add column units_scheduled bigint default 0;
-
-ALTER TABLE schedule_event ALTER COLUMN sale_item_id DROP NOT NULL;
-
-alter table sale add column units_assigned bigint default 0;
-
 --Set scheduleEvent.item
 run /scheduleEvent/migrate
 
