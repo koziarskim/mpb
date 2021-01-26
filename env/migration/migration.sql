afed0a655bd397c2161a5dc802090da0116e6806
Marcin Koziarski <koziarskim@yahoo.com>
2021-01-25 23:36:35 -0600

    Deploy

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -5,11 +5,4 @@ set schema 'shared';
 --End SHARED
 set schema 'y2020';
 
-alter table sale_item add column department varchar(50);
-alter table sale_item add column expiration date;
-alter table address add column location_name varchar(50);
-alter table address rename column notes to notes_delete;
-alter table address add column notes varchar(70);
-alter table sale_item add column pcr boolean default false;
-alter table sale add column pcr boolean default false;
 ---------------------------------
