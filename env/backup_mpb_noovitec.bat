2a079c7d69090754d6b42747b3e5154ae5cf7b57
Marcin Koziarski <koziarskim@yahoo.com>
2021-02-04 11:23:55 -0600

    Update backup to use export instead of backup.

--- env/backup_mpb_noovitec.bat
+++ env/backup_mpb_noovitec.bat
@@ -5,35 +5,35 @@ for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (set datestr=%%l_%%j_%%k)
 rem for /f "tokens=1-4 delims=/:." %%a in ("%TIME%") do (set timestr=%%a_%%b_%%c)
 
 set ARG=%1%
-set BACKUP=false
+set EXPORT=false
 set IMPORT=false
 set DATE_FORMAT=%datestr%
-set BACKUP_DIR=C:\Users\kozia\postgres\backups\noovitec
-set SCHEMA_FILE_SHARED=%BACKUP_DIR%\schema_shared_%DATE_FORMAT%.sql
-set SCHEMA_FILE_2020=%BACKUP_DIR%\schema_2020_%DATE_FORMAT%.sql
-set DATA_FILE_SHARED=%BACKUP_DIR%\data_shared_%DATE_FORMAT%.sql
-set DATA_FILE_2020=%BACKUP_DIR%\data_2020_%DATE_FORMAT%.sql
+set EXPORT_DIR=C:\Users\kozia\postgres\backups\noovitec
+set SCHEMA_FILE_SHARED=%EXPORT_DIR%\schema_shared_%DATE_FORMAT%.sql
+set SCHEMA_FILE_2020=%EXPORT_DIR%\schema_2020_%DATE_FORMAT%.sql
+set DATA_FILE_SHARED=%EXPORT_DIR%\data_shared_%DATE_FORMAT%.sql
+set DATA_FILE_2020=%EXPORT_DIR%\data_2020_%DATE_FORMAT%.sql
 set DB_NAME=mpb_%DATE_FORMAT%
 echo Schemas: %SCHEMA_FILE_SHARED% , %SCHEMA_FILE_2019% , %SCHEMA_FILE_2020%
 echo Data: %DATA_FILE_SHARED% , %DATA_FILE_2019% , %DATA_FILE_2020%
 echo DB name: %DB_NAME%
 
 if "%ARG%"=="" (
-	set BACKUP=true
+	set EXPORT=true
 )
-if "%ARG%"=="backup" (
-	set BACKUP=true
+if "%ARG%"=="export" (
+	set EXPORT=true
 )
 if "%ARG%"=="import" (
 	set IMPORT=true
 )
 if "%ARG%"=="full" (
-	set BACKUP=true
+	set EXPORT=true
 	set IMPORT=true
 )
-:backup
-if "%BACKUP%" == "true" (
-	echo Processing Backup...
+:export
+if "%EXPORT%" == "true" (
+	echo Processing Export...
 	if exist %SCHEMA_FILE_SHARED% (echo Schema File already exist %SCHEMA_FILE_SHARED% && GOTO import)
 		"C:\Program Files (x86)\pgAdmin 4\v4\runtime\pg_dump.exe" --file %SCHEMA_FILE_SHARED% --host "mpb.noovitec.com" --port "5432" --username "postgres" --verbose --format=p --create --inserts --column-inserts --schema-only --schema "shared" "mpb"
 		"C:\Program Files (x86)\pgAdmin 4\v4\runtime\pg_dump.exe" --file %SCHEMA_FILE_2020% --host "mpb.noovitec.com" --port "5432" --username "postgres" --verbose --format=p --create --inserts --column-inserts --schema-only --schema "y2020" "mpb"
@@ -43,15 +43,15 @@ if "%BACKUP%" == "true" (
 :import
 if "%IMPORT%" == "true" (
 	echo Processing Import...
-	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %SCHEMA_FILE_SHARED% > %BACKUP_DIR%\clean_schema_shared.sql
-	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %SCHEMA_FILE_2020% > %BACKUP_DIR%\clean_schema_2020.sql
-	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %DATA_FILE_SHARED% > %BACKUP_DIR%\clean_data_shared.sql
-	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %DATA_FILE_2020% > %BACKUP_DIR%\clean_data_2020.sql
+	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %SCHEMA_FILE_SHARED% > %EXPORT_DIR%\clean_schema_shared.sql
+	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %SCHEMA_FILE_2020% > %EXPORT_DIR%\clean_schema_2020.sql
+	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %DATA_FILE_SHARED% > %EXPORT_DIR%\clean_data_shared.sql
+	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %DATA_FILE_2020% > %EXPORT_DIR%\clean_data_2020.sql
 	createdb -h localhost -p 5432 -U postgres %DB_NAME% || (GOTO end)
-	psql -U postgres -d %DB_NAME% < %BACKUP_DIR%\clean_schema_shared.sql
-	psql -U postgres -d %DB_NAME% < %BACKUP_DIR%\clean_schema_2020.sql
-	psql -U postgres -d %DB_NAME% < %BACKUP_DIR%\clean_data_shared.sql
-	psql -U postgres -d %DB_NAME% < %BACKUP_DIR%\clean_data_2020.sql
+	psql -U postgres -d %DB_NAME% < %EXPORT_DIR%\clean_schema_shared.sql
+	psql -U postgres -d %DB_NAME% < %EXPORT_DIR%\clean_schema_2020.sql
+	psql -U postgres -d %DB_NAME% < %EXPORT_DIR%\clean_data_shared.sql
+	psql -U postgres -d %DB_NAME% < %EXPORT_DIR%\clean_data_2020.sql
 )
 :end
 echo Done!
