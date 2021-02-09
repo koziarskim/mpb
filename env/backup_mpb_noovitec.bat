8d167edf950661db2762566ed15c19f3c6793aa5
Marcin Koziarski <koziarskim@yahoo.com>
2021-02-09 15:54:57 -0600

    Remove shared schema from backup script.

--- env/backup_mpb_noovitec.bat
+++ env/backup_mpb_noovitec.bat
@@ -9,13 +9,11 @@ set EXPORT=false
 set IMPORT=false
 set DATE_FORMAT=%datestr%
 set EXPORT_DIR=C:\Users\kozia\postgres\backups\noovitec
-set SCHEMA_FILE_SHARED=%EXPORT_DIR%\schema_shared_%DATE_FORMAT%.sql
 set SCHEMA_FILE_2020=%EXPORT_DIR%\schema_2020_%DATE_FORMAT%.sql
-set DATA_FILE_SHARED=%EXPORT_DIR%\data_shared_%DATE_FORMAT%.sql
 set DATA_FILE_2020=%EXPORT_DIR%\data_2020_%DATE_FORMAT%.sql
 set DB_NAME=mpb_%DATE_FORMAT%
-echo Schemas: %SCHEMA_FILE_SHARED% , %SCHEMA_FILE_2019% , %SCHEMA_FILE_2020%
-echo Data: %DATA_FILE_SHARED% , %DATA_FILE_2019% , %DATA_FILE_2020%
+echo Schemas: %SCHEMA_FILE_2020%
+echo Data: %DATA_FILE_2020%
 echo DB name: %DB_NAME%
 
 if "%ARG%"=="" (
@@ -34,23 +32,17 @@ if "%ARG%"=="full" (
 :export
 if "%EXPORT%" == "true" (
 	echo Processing Export...
-	if exist %SCHEMA_FILE_SHARED% (echo Backup Failed! Schema File already exist %SCHEMA_FILE_SHARED% && GOTO end)
-		"C:\Program Files (x86)\pgAdmin 4\v4\runtime\pg_dump.exe" --file %SCHEMA_FILE_SHARED% --host "mpb.noovitec.com" --port "5432" --username "postgres" --verbose --format=p --create --inserts --column-inserts --schema-only --schema "shared" "mpb"
+	if exist %SCHEMA_FILE_2020% (echo Backup Failed! Schema File already exist %SCHEMA_FILE_2020% && GOTO end)
 		"C:\Program Files (x86)\pgAdmin 4\v4\runtime\pg_dump.exe" --file %SCHEMA_FILE_2020% --host "mpb.noovitec.com" --port "5432" --username "postgres" --verbose --format=p --create --inserts --column-inserts --schema-only --schema "y2020" "mpb"
-		"C:\Program Files (x86)\pgAdmin 4\v4\runtime\pg_dump.exe" --exclude-table-data "doc_content" --file %DATA_FILE_SHARED% --host "mpb.noovitec.com" --port "5432" --username "postgres" --verbose --format=p --create --inserts --column-inserts --data-only --schema "shared" "mpb"
 		"C:\Program Files (x86)\pgAdmin 4\v4\runtime\pg_dump.exe" --exclude-table-data "doc_content" --file %DATA_FILE_2020% --host "mpb.noovitec.com" --port "5432" --username "postgres" --verbose --format=p --create --inserts --column-inserts --data-only --schema "y2020" "mpb"
 )
 :import
 if "%IMPORT%" == "true" (
 	echo Processing Import...
-	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %SCHEMA_FILE_SHARED% > %EXPORT_DIR%\clean_schema_shared.sql
 	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %SCHEMA_FILE_2020% > %EXPORT_DIR%\clean_schema_2020.sql
-	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %DATA_FILE_SHARED% > %EXPORT_DIR%\clean_data_shared.sql
 	findstr /v /b /c:"SET " /c:"CREATE DATABASE" /c:"ALTER DATABASE" /c:"\connect" /c:"REVOKE ALL" /c:"GRANT ALL" /c:"SELECT pg_catalog.set_config('search_path'" %DATA_FILE_2020% > %EXPORT_DIR%\clean_data_2020.sql
 	createdb -h localhost -p 5432 -U postgres %DB_NAME% || (echo Backup Failed DB already exist && GOTO end)
-	psql -U postgres -d %DB_NAME% < %EXPORT_DIR%\clean_schema_shared.sql
 	psql -U postgres -d %DB_NAME% < %EXPORT_DIR%\clean_schema_2020.sql
-	psql -U postgres -d %DB_NAME% < %EXPORT_DIR%\clean_data_shared.sql
 	psql -U postgres -d %DB_NAME% < %EXPORT_DIR%\clean_data_2020.sql
 )
 :end
