c2c038abda5b979e002e0f056f82cddf7292bce9
Marcin Koziarski <koziarskim@yahoo.com>
2021-02-12 09:39:17 -0600

    DC to DC/Location Name.

--- vue/src/components/modals/AddressModal.vue
+++ vue/src/components/modals/AddressModal.vue
@@ -2,16 +2,20 @@
   <b-container fluid>
     <b-modal centered size="lg" v-model="visible" :hide-header="true" :hide-footer="true">
       <b-row>
-				<b-col cols=4>
-					<label class="top-label">Address Distribution Center</label>
-					<input class="form-control" maxlength="20" type="text" v-model="address.dc">
-				</b-col>
-				<b-col v-if="addressType=='FRG'">
-					<label class="top-label">Keep it? </label><br/>
-					<input type="checkbox" v-model="address.visible">
-				</b-col>
+		<b-col cols=4>
+			<label class="top-label">Distribution Center/Location Name</label>
+			<input class="form-control" maxlength="20" type="text" v-model="address.dc">
+		</b-col>
+		<b-col cols=4>
+			<label class="top-label">Location ID:</label>
+			<input class="form-control" type="tel" v-model="address.locationName"/>
+		</b-col>
+		<b-col cols=1 v-if="addressType=='FRG'">
+			<label class="top-label">Keep? </label><br/>
+			<input type="checkbox" v-model="address.visible">
+		</b-col>
         <b-col>
-          <div style="text-align: right;">
+          <div style="text-align: right; margin-top: 20px">
             <b-button style="margin: 0 2px 0 2px" @click="closeModal()">Close</b-button>
             <b-button style="margin: 0 2px 0 2px" @click="saveModal()" variant="success">Save</b-button>
           </div>
@@ -19,14 +23,14 @@
       </b-row>
 			<b-row>
 				<b-col cols=6>
-					<label class="top-label">Address Line</label>
-					<input class="form-control" type="tel" v-model="address.line">
+					<label class="top-label">Street</label>
+					<input class="form-control" type="tel" v-model="address.street">
 				</b-col>
 			</b-row>
 			<b-row>
 				<b-col cols=6>
-					<label class="top-label">Street</label>
-					<input class="form-control" type="tel" v-model="address.street">
+					<label class="top-label">Additional Line</label>
+					<input class="form-control" type="tel" v-model="address.line">
 				</b-col>
 			</b-row>
 			<b-row>
@@ -47,10 +51,6 @@
 				<b-col cols=5>
 					<label class="top-label">Phone:</label>
 					<input class="form-control" type="tel" v-model="address.phone"/>
-				</b-col>
-				<b-col cols=5>
-					<label class="top-label">Location ID:</label>
-					<input class="form-control" type="tel" v-model="address.locationName"/>
 				</b-col>
 			</b-row>
 			<b-row>
