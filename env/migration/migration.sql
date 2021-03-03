582d1179071a73e42465eb4fb966d5c9f5cae8c6
Marcin Koziarski <koziarskim@yahoo.com>
2021-03-03 09:29:13 -0600

    Deployed.

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -1,6 +1,3 @@
 -- Schema public.
 
-alter table packaging add column attachment_id bigint;
-alter table packaging add CONSTRAINT fk_packaging_attachment_id FOREIGN KEY (attachment_id) REFERENCES attachment (id);
-ALTER TABLE shipment ALTER COLUMN status SET NOT NULL;
 ---------------------------------
