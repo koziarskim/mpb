af2a6b01f5783bc108a19a35933ef460325c80d4
Marcin Koziarski <koziarskim@yahoo.com>
2021-03-05 16:15:10 -0600

    Deploy.

--- env/migration/migration.sql
+++ env/migration/migration.sql
@@ -1,6 +1,4 @@
 -- Schema public.
 
-update customer set label_type = 'EDI' where label_type = 'UCC_128';
-update customer set label_type = 'MIMS' where label_type = 'SELF_GEN';
 
 ---------------------------------
